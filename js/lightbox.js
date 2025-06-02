40d7a5f1cd94838dc37d6f956f3c859fd5b8c713
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-06-02 10:35:03 +0330

    HTML again

---
+++ js/lightbox.js
@@ -0,0 +1,3 @@
+// Get lightbox.js from https://lokeshdhakar.com/projects/lightbox2/
+// Or use this CDN link in your HTML instead:
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
